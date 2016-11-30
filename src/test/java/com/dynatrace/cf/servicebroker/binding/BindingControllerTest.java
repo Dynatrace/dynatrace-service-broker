21e3fb16c47f243667e9dd0fdef4cd3577418c38
Alois Mayr <alois.mayr@dynatrace.com>
2016-11-30 22:26:58 +0100

    Introduce `apiary` field and remove deprecated `tenanttoken` and `endpoint`

--- src/test/java/com/dynatrace/cf/servicebroker/binding/BindingControllerTest.java
+++ src/test/java/com/dynatrace/cf/servicebroker/binding/BindingControllerTest.java
@@ -36,7 +36,7 @@ public final class BindingControllerTest extends AbstractControllerTest {
         this.mockMvc.perform(put("/v2/service_instances/0/service_bindings/1").content(payload()).contentType(MediaType
                 .APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.credentials.tenant").exists());
+                .andExpect(jsonPath("$.credentials.environmentid").exists());
     }
 
     @Test
